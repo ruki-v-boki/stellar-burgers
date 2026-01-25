6963dc9e788a6547f5ac06c61ce4c8961d59dd4d
ruki_v_boki <maksiimivanoov@yandex.ru>
2026-01-25 09:06:31 +0300

    добавил клики по ссылкам из шапки

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const ESLintPlugin = require('eslint-webpack-plugin');
+// const ESLintPlugin = require('eslint-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
 
@@ -48,9 +48,9 @@ module.exports = {
     ]
   },
   plugins: [
-    new ESLintPlugin({
-      extensions: ['.js', '.jsx', '.ts', '.tsx']
-    }),
+    // new ESLintPlugin({
+    //   extensions: ['.js', '.jsx', '.ts', '.tsx']
+    // }),
     new HtmlWebpackPlugin({
       template: './public/index.html'
     }),
